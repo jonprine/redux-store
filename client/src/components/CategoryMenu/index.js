e4466f4b9158cec84c2d567ff82a36213ec5cedd
Vox Humana <jonprine@gmail.com>
2020-11-03 08:53:33 -0600

    updated categorymenu, productlist, productitem for redux

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -5,11 +5,13 @@ import {
 } from "../../utils/actions";
 import { useQuery } from "@apollo/react-hooks";
 import { QUERY_CATEGORIES } from "../../utils/queries";
-import { useStoreContext } from "../../utils/GlobalState";
 import { idbPromise } from "../../utils/helpers";
+import { useSelector, useDispatch } from "react-redux";
 
 function CategoryMenu() {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector(state => state);
+
+  const dispatch = useDispatch();
 
   const { categories } = state;
 
